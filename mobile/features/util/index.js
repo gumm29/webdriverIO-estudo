c60e4ac5227be006a184f0720f73979ef19e36cb
Gustavo Munis <gustavomm29@hotmail.com>
2022-11-30 16:19:04 -0300

    pegar nome de arquivo rodado

--- mobile/features/util/index.js
+++ mobile/features/util/index.js
@@ -2,9 +2,16 @@ const yaml = require('js-yaml')
 const fs   = require('fs')
 
 get_screen_mappings = (screen) => {
-    let plataforma = process.env.PLATAFORMA.toLowerCase()
+    let plataforma = get_runnig_filename()
     dir = `./features/pageobjects/elements/${plataforma}/screen_mapping_${screen}.yml`
     return yaml.load(fs.readFileSync(dir))
-}  
+}
+
+get_runnig_filename = () => {
+    var name = process.argv.toString()
+    name = name.split(',')
+    name = name[3].split('.')
+    return name[0]
+}
 
 module.exports = get_screen_mappings
